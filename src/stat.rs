6221e628e4483366301677cda64e871f4281f1db
Aaron Trent <novacrazy@gmail.com>
2017-04-24 19:44:34 -0500

    Add f32 variation of gaussian function

--- src/stat.rs
+++ src/stat.rs
@@ -1,7 +1,14 @@
 //! Probability Distribution Functions
 
+use num_traits::Float;
+
 pub fn gaussian_dot_pdf(x: f64, width: f64, hardness: f64) -> f64 {
     let exponent = (x / (width * 0.5)).powf(2.0f64.powf(hardness));
 
     ::std::f64::consts::E.powf(-exponent)
+}
+
+#[inline]
+pub fn gaussian_dot_pdf_32(x: f32, width: f32, hardness: f32) -> f32 {
+    gaussian_dot_pdf(x as f64, width as f64, hardness as f64) as f32
 }
